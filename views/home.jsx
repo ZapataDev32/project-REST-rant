c2c37fcae1797712cb56b24abcc642f230d49a35
Ghostbitstudios <126435713+Ghostbitstudios@users.noreply.github.com>
2024-02-06 12:19:44 -0800

    adding bootstrap gris to index page

--- views/home.jsx
+++ views/home.jsx
@@ -6,8 +6,15 @@ function home () {
       <Def>
           <main>
               <h1>HOME</h1>
+              <div>
+                <img src="/images/chad-montano-eeqbbemH9-c-unsplash.jpg" alt='Pankcakes'></img>
+                <div>
+                Photo by <a href="https://unsplash.com/@briewilly?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Chad Montano</a> on <a href="https://unsplash.com/photos/baked-pancakes-eeqbbemH9-c?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+  
+                </div>
+              </div>
               <a href='/places'>
-                <button className='btn-primary'>Places Page</button>
+                <button className='btn btn-primary'>Places Page</button>
               </a>
           </main>
       </Def>
