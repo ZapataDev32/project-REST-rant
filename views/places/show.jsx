dc57e4cd583b45dcb983f4d46a1c68c8cb8a8c57
Ghostbitstudios <126435713+Ghostbitstudios@users.noreply.github.com>
2024-02-07 21:45:33 -0800

    added stuff, but im not done

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,14 +1,39 @@
 const React = require('react')
 const Def = require('../default')
 
-function show () {
+function show ({ place }) {
+
+    const cuisinesBadges = place.cuisine.split(',').map((cuisine) => {
+        return <span key={cuisine} className='badge rounded-pill text-bg-info me-2'> {cuisine}</span>
+    })
     return (
         <Def>
-          <main>
-            <h1>Show Page</h1>
+          <main className='container'>
+            <div className='row align-items-start'>
+                <div className='col'>
+                    <img src={place.pic} alt={place.name}/>
+                </div>
+            </div>
+            <div className='col'>
+                <h1>{place.name}</h1>
+                <p>{cuisinesBadges}</p>
+            </div>
+            <div className='row align-items-center'>
+                <div className='col'>
+                    <a href='' className='btn btn-warning'>
+                        Edit
+                    </a>
+                    <form action={`/places/${id}?_method=DELETE`} method='POST'>
+                        <button type='submit' className='btn btn-danger'>
+                            Delete
+                        </button>
+                    </form>
+                </div>
+            </div>
           </main>
         </Def>
     )
 }
 
 module.exports = show
+
