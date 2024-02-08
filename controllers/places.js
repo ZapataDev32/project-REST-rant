fb6a14435a53860e7eb6f336ebfff8ee3190547f
Ghostbitstudios <126435713+Ghostbitstudios@users.noreply.github.com>
2024-02-07 18:22:40 -0800

    new places cab be added

--- controllers/places.js
+++ controllers/places.js
@@ -9,14 +9,26 @@ router.get('/new', (req, res) => {
     res.render('places/new')
 })
 
-router.get('/:id', (req,res) => {
-    res.render('places/show', {place})
-})
+// router.get('/:id', (req,res) => {
+//     res.render('places/show', {places})
+// })
   
 router.post('/', (req, res) => {
-    console.log(req.body)
-    res.send('POST /places')
-})
+    // console.log(req.body)
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
+  })
+  
   
 
 // router.post('/', (req, res) => {
