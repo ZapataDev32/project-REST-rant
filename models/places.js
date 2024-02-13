e8e051bbf747ca6f570b50b8df17de579cab9899
Ghostbitstudios <126435713+Ghostbitstudios@users.noreply.github.com>
2024-02-12 18:54:44 -0800

    added moongoose and changed places

--- models/places.js
+++ models/places.js
@@ -1,14 +1,27 @@
 
-module.exports = [{
-    name: 'H-Thai-ML',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Thai, Pan-Asian',
-    pic: '/images/nathan-dumlao-Y3AqmbmtLQI-unsplash.jpg'
-  }, {
-    name: 'Coding Cat Cafe',
-    city: 'Phoenix',
-    state: 'AZ',
-    cuisines: 'Coffee, Bakery',
-    pic: '/images/shawnanggg-nmpW_WwwVSc-unsplash.jpg'
-  }]
+// module.exports = [{
+//     name: 'H-Thai-ML',
+//     city: 'Seattle',
+//     state: 'WA',
+//     cuisines: 'Thai, Pan-Asian',
+//     pic: '/images/nathan-dumlao-Y3AqmbmtLQI-unsplash.jpg'
+//   }, {
+//     name: 'Coding Cat Cafe',
+//     city: 'Phoenix',
+//     state: 'AZ',
+//     cuisines: 'Coffee, Bakery',
+//     pic: '/images/shawnanggg-nmpW_WwwVSc-unsplash.jpg'
+//   }]
+
+const mongoose = require('mongoose')
+
+const placeSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  pic: String,
+  cuisines: { type: String, required: true },
+  city: { type: String, default: 'Anytown' },
+  state: { type: String, default: 'USA' },
+  founded: Number
+})
+
+module.exports = mongoose.model('Place', placeSchema)
