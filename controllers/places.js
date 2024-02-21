ca3fbeaab842b895d90ed2cb899e89044523c8fc
Ghostbitstudios <126435713+Ghostbitstudios@users.noreply.github.com>
2024-02-21 15:32:17 -0800

    added comment section but the rave does not work

--- controllers/places.js
+++ controllers/places.js
@@ -1,103 +1,4 @@
-// const router = require('express').Router()
-// const places = require('../models/places.js')
 
-// router.get('/', (req, res) => {
-//     res.render('places/index', {places}) 
-// })
-
-// router.get('/new', (req, res) => {
-//     res.render('places/new')
-// })
-
-// router.get('/:id', (req, res) => {
-//     let id = Number(req.params.id)
-//     if (isNaN(id)) {
-//       res.render('error404')
-//     }
-//     else if (!places[id]) {
-//       res.render('error404')
-//     }
-//     else {
-//       res.render('places/show', { place: places[id],id: id})
-//     }
-// })
-  
-// router.post('/', (req, res) => {
-//     if (!req.body.pic) {
-//       // Default image if one is not provided
-//       req.body.pic = 'http://placekitten.com/400/400'
-//     }
-//     if (!req.body.city) {
-//       req.body.city = 'Anytown'
-//     }
-//     if (!req.body.state) {
-//       req.body.state = 'USA'
-//     }
-//     places.push(req.body)
-//     res.redirect('/places')
-// })
-  
-  
-// router.delete('/:id', (req, res) => {
-//     let id = Number(req.params.id)
-//     if (isNaN(id)) {
-//       res.render('error404')
-//     }
-//     else if (!places[id]) {
-//       res.render('error404')
-//     }
-//     else {
-//         places.splice(id, 1)
-//         res.redirect('/places')
-//     }
-// })
-
-// router.get('/:id/edit', (req, res) => {
-//     let id = Number(req.params.id)
-//     if (isNaN(id)) {
-//         res.render('error404')
-//     }
-//     else if (!places[id]) {
-//         res.render('error404')
-//     }
-//     else {
-//       res.render('places/edit', { place: places[id], id: id})
-//     }
-// })
-
-// router.put('/:id', (req, res) => {
-//     let id = Number(req.params.id)
-//     if (isNaN(id)) {
-//         res.render('error404')
-//     }
-//     else if (!places[id]) {
-//         res.render('error404')
-//     }
-//     else {
-//         // Dig into req.body and make sure data is valid
-//         if (!req.body.pic) {
-//             // Default image if one is not provided
-//             req.body.pic = 'http://placekitten.com/400/400'
-//         }
-//         if (!req.body.city) {
-//             req.body.city = 'Anytown'
-//         }
-//         if (!req.body.state) {
-//             req.body.state = 'USA'
-//         }
-  
-//         // Save the new data into places[id]
-//         places[id] = req.body
-//         res.redirect(`/places/${id}`)
-//     }
-//   })
-  
-  
-  
-  
-
-
-// module.exports = router
 
 
 const router = require('express').Router()
@@ -141,42 +42,66 @@ router.post('/', (req, res) => {
         }
     })
   })
-  
 
 router.get('/new', (req, res) => {
   res.render('places/new')
 })
 
 router.get('/:id', (req, res) => {
-    db.Place.findById(req.params.id)
-    .then(place => {
-        res.render('places/show', { place })
-    })
-    .catch(err => {
-        console.log('err', err)
-        res.render('error404')
-    })
+  db.Place.findById(req.params.id)
+  .populate('comments')
+  .then(place => {
+      // console.log('COMMENTS:',place.comments)
+      res.render('places/show', { place })
+  })
+  .catch(err => {
+      console.log('err', err)
+      res.render('error404')
+  })
+})
+
+router.post('/:id/comment', (req, res) => {
+  console.log(req.body)
+  db.Place.findById(req.params.id)
+  .then(place => {
+      db.Comment.create(req.body)
+      .then(comment => {
+          place.comments.push(comment.id)
+          place.save()
+          .then(() => {
+              res.redirect(`/places/${req.params.id}`)
+          })
+      })
+      .catch(err => {
+          res.render('error404')
+      })
+  })
+  .catch(err => {
+      res.render('error404')
+  })
 })
 
 
-router.put('/:id', (req, res) => {
-  res.send('PUT /places/:id stub')
-})
 
-router.delete('/:id', (req, res) => {
-  res.send('DELETE /places/:id stub')
-})
 
-router.get('/:id/edit', (req, res) => {
-  res.send('GET edit form stub')
-})
+// router.put('/:id', (req, res) => {
+//   res.send('PUT /places/:id stub')
+// })
 
-router.post('/:id/rant', (req, res) => {
-  res.send('GET /places/:id/rant stub')
-})
+// router.delete('/:id', (req, res) => {
+//   res.send('DELETE /places/:id stub')
+// })
 
-router.delete('/:id/rant/:rantId', (req, res) => {
-    res.send('GET /places/:id/rant/:rantId stub')
-})
+// router.get('/:id/edit', (req, res) => {
+//   res.send('GET edit form stub')
+// })
+
+// router.post('/:id/rant', (req, res) => {
+//   res.send('GET /places/:id/rant stub')
+// })
+
+// router.delete('/:id/rant/:rantId', (req, res) => {
+//     res.send('GET /places/:id/rant/:rantId stub')
+// })
 
 module.exports = router
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,13 +2,32 @@ const React = require('react')
 const Def = require('../default')
 
 function show (data) {
+    let comments = (
+        <h3 className='inactive'>
+            No comments yet Bro!
+        </h3>
+    )
+    if (data.place.comments.length) {
+        comments = data.place.comments.map(c => {
+          return (
+            <div className="border">
+              <h2 className="rant">{c.rant ? 'Rant! ' : 'Rave! '}</h2>
+              <h4>{c.content}</h4>
+              <h3>
+                <stong>- {c.author}</stong>
+              </h3>
+              <h4>Rating: {c.stars}</h4>
+            </div>
+          )
+        })
+    }
 
     const cuisinesBadges = data.place.cuisines.split(',').map((cuisine) => {
         return <span key={cuisine} className='badge rounded-pill text-bg-info me-2'> {cuisine}</span>
     })
     return (
         <Def>
-          <main className='container'>
+          <main className='container text-center'>
             <div className='row align-items-start'>
                 <div className='col'>
                     <img src={data.place.pic} alt={data.place.name}/>
@@ -16,32 +35,28 @@ function show (data) {
                         Located in {data.place.city}, {data.place.state}
                     </h3>
                 </div>
-            </div>
-            <div className='col'>
-                <h1>{data.place.name}</h1>
-                <p>{cuisinesBadges}</p>
-                <p className='badge rounded-pill text-bg-info me-2'>{`${data.place.city}, ${data.place.state}`}</p>
-            </div>
-            <h2>Description</h2>
-            <h3>
-                {data.place.showEstablished()}
-            </h3>
-            <h4>
-                Serving {data.place.cuisines}
-            </h4>
-            <div className='row align-items-center'>
-                <div className='col'>
+                <div className="col">
+                    <h1>{data.place.name}</h1>
+                    <p>{cuisinesBadges}</p>
+                    <p className='badge rounded-pill text-bg-info me-2'>{`${data.place.city}, ${data.place.state}`}</p> 
+                    <h2>Description</h2>
+                    <h3>
+                        {data.place.showEstablished()}
+                    </h3>
+                    <h4>
+                        Serving {data.place.cuisines}
+                    </h4>
                     <a href={`/places/${data.id}/edit`} className='btn btn-warning'>
                         Edit
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
-  <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-  <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
-</svg>
-                    </a>
-                    <form method= "POST" action={`/places/${data.id}?_method=DELETE`}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-square" viewBox="0 0 16 16">
+                        <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                        <path fillRule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
+                        </svg>
+                        </a>
+                        <form method= "POST" action={`/places/${data.id}?_method=DELETE`}>
                         <button type='submit' className='btn btn-danger'>
                             Delete
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
   <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
   <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
 </svg>
@@ -49,6 +64,40 @@ function show (data) {
                     </form>
                 </div>
             </div>
+            <div className='col'>
+
+            </div>
+
+            <div className='row'>
+                <div className='form-group col-sm-12'>
+                    <h2>Comments</h2>
+                    {comments}
+                </div>
+            </div>
+            <h2>Got Your own Rant or Rave?</h2>
+            <form action={`/places/${data.id}/comment`} method='POST'>
+                <div className='row'>
+                    <div className='form-group col-sm-12'>
+                        <label htmlFor='content'>Content</label>
+                        <textarea id='content' name='content' className='form-control'></textarea>
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='form-group col-sm-4'>
+                        <label htmlFor='author'>Author</label>
+                        <input id='author' name='author' className='form-control'></input>
+                    </div>
+                    <div className='form-group col-sm-4'>
+                        <label htmlFor='stars'>Star Rating</label>
+                        <input type='range' step="0.5" min="1" max="5" id="stars" name='stars' className='form-control'></input>
+                    </div>
+                    <div className='form-group col-sm-1'>
+                        <label htmlFor="rant" className='mt-4'>Rant?</label>
+                        <input type="checkbox" id="rant" name="rant" className="form-check-input mt-4"></input>
+                    </div>
+                </div>
+                <input type='submit' className='btn btn-primary' value="Add Comment"></input>
+            </form>
           </main>
         </Def>
     )
